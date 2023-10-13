caf6e750da55528b47a0821af6db7c6d4f7b6b2c
SobithaRani-G <143074647+SobithaRani-G@users.noreply.github.com>
2023-10-13 15:25:38 +0530

    Add files via upload

--- Memory Allocations/worst_fit.c
+++ Memory Allocations/worst_fit.c
@@ -2,9 +2,9 @@
 #define max 25
 void main()
 {
-	int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0;
+	int frag[max],b[max],f[max],i,j,nb,nf,temp;
 	static int bf[max],ff[max];
-	printf("\n\tMemory Management Scheme - Worst Fit");
+	printf("\n\tMemory Management Scheme - First Fit");
 	printf("\nEnter the number of blocks:");
 	scanf("%d",&nb);
 	printf("Enter the number of files:");
@@ -23,23 +23,22 @@ void main()
 	}
 	for(i=1;i<=nf;i++)
 	{
-	for(j=1;j<=nb;j++)
-	{
-		if(bf[j]!=1)
+		for(j=1;j<=nb;j++)
 		{
-		temp=b[j]-f[i];
-		if(temp>=0)
-		if(highest<temp)
-		{
-			frag[i]=highest;
-			bf[ff[i]]=1;
-			highest=0;
+			if(bf[j]!=1)
+			{
+			temp=b[j]-f[i];
+			if(temp>=0)
+			{
+			ff[i]=j;
+			break;
+			}
+			}
 		}
-		}	
-	}
-	ff[i]=j; highest=temp;
-	}
-	printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement");
+	frag[i]=temp;
+	bf[ff[i]]=1;
+	}	
+	printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
 	for(i=1;i<=nf;i++)
 	printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 }
