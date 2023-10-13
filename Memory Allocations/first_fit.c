b9a3aca004afa35281f02166b1da1faf8d510b6c
SobithaRani-G <143074647+SobithaRani-G@users.noreply.github.com>
2023-10-13 19:53:02 +0530

    first_fit.c

--- Memory Allocations/first_fit.c
+++ Memory Allocations/first_fit.c
@@ -2,20 +2,20 @@
 #define max 25
 void main()
 {
-	int frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0;
+	int frag[max],b[max],f[max],i,j,nf,nb,temp;
 	static int bf[max],ff[max];
-	printf("\n\tMemory Management Scheme - Worst Fit");
-	printf("\nEnter the number of blocks:");
+	printf("\n\tMemory management scheme - First Fit");
+	printf("\nEnter no of blocks:");
 	scanf("%d",&nb);
-	printf("Enter the number of files:");
+	printf("Enter no of files:");
 	scanf("%d",&nf);
-	printf("\nEnter the size of the blocks:-\n");
+	printf("\nEnter the size of blocks:-\n");
 	for(i=1;i<=nb;i++)
 	{
 		printf("Block %d:",i);
 		scanf("%d",&b[i]);
 	}
-	printf("Enter the size of the files :-\n");
+	printf("\nEnter the size of the files:-\n");
 	for(i=1;i<=nf;i++)
 	{
 		printf("File %d:",i);
@@ -23,23 +23,22 @@ void main()
 	}
 	for(i=1;i<=nf;i++)
 	{
-	for(j=1;j<=nb;j++)
-	{
-		if(bf[j]!=1)
+		for(j=1;j<=nb;j++)
 		{
-		temp=b[j]-f[i];
-		if(temp>=0)
-		if(highest<temp)
-		{
-			frag[i]=highest;
-			bf[ff[i]]=1;
-			highest=0;
+			if(bf[j]!=1)
+			{
+				temp=b[j]-f[i];
+				if(temp>=0)
+				{
+					ff[i]=j;
+					break;
+				}
+			}
 		}
-		}	
+		frag[i]=temp;
+		bf[ff[i]]=1;
 	}
-	ff[i]=j; highest=temp;
-	}
-	printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement");
+	printf("\nFile_no:\tFile_size:\tBlock No:\tBlock_size:\tFragment:\n");
 	for(i=1;i<=nf;i++)
-	printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
+	printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",i,f[i],ff[i],b[ff[i]],frag[i]);
 }
